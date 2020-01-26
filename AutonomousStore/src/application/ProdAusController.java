d34c084619c26e4fd7bc106d0cc9d62191800367
lucas <lucas@192.168.0.143>
2020-01-26 19:16:40 +0100

    Anpassungen

--- AutonomousStore/src/application/ProdAusController.java
+++ AutonomousStore/src/application/ProdAusController.java
@@ -1,22 +1,70 @@
 package application;
 import javafx.event.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class ProdAusController {
 
-	@FXML
-	private Label lblInfoProdAus ;
+	
 
-	    @FXML
-	    private ImageView imgZigaretten;
+    @FXML
+    private ImageView imgZigaretten;
 
+    @FXML
+    private Button btnLottoSpielen;
+
+    @FXML
+    private Button btnZigarettenKaufen;
+
+    @FXML
+    private Button btnLebensmittelKaufen;
+
+    @FXML
+    private Label lblInfoProdAus;
 	    @FXML
 	    void btnLottoSpielen_clicked (ActionEvent event) {
-	    	lblInfoProdAus.setText("Hello World");
+try {
+				
+	    		Stage StageRegist = (Stage) ((Node)event.getSource()).getScene().getWindow();
+				Parent root = FXMLLoader.load(getClass().getResource("LottoSpielen.fxml"));
+				Scene scene = new Scene(root);
+				StageRegist.setScene(scene);
+				StageRegist.show();
+			} 
+			
+			catch(Exception e) {
+				e.printStackTrace();
+			}
 	    }
 
+	    @FXML
+	    void btnZigarettenKaufen_clicked(ActionEvent event) {
+try {
+				
+	    		Stage StageRegist = (Stage) ((Node)event.getSource()).getScene().getWindow();
+				Parent root = FXMLLoader.load(getClass().getResource("ZigarettenKaufen.fxml"));
+				Scene scene = new Scene(root);
+				StageRegist.setScene(scene);
+				StageRegist.show();
+			} 
+			
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+	    }
+
+	    @FXML
+	    void btnLebensmittelKaufen_clicked(ActionEvent event) {
+// TODO FXML fuer Lebensmittel erstellen
+	    }
+	    
 	}
 	
 	
--- AutonomousStore/src/application/ZigarettenController.java
+++ AutonomousStore/src/application/ZigarettenController.java
@@ -1,5 +1,22 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ZigarettenController {
 
+    @FXML
+    private TextField txtMengeproAuswahl;
+
+    @FXML
+    private Button btnZigarettenWarenkorbHinzu;
+
+    @FXML
+    void btnZigarettenWarenkorbHinzu_clicked(ActionEvent event) {
+    	txtMengeproAuswahl.setText("Hello World");
+    }
+
 }
+
