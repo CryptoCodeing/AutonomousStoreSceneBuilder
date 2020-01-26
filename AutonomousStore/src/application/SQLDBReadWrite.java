7b841b7e594b096bbf9f3c01c0a35362874df62a
lucas <lucas@192.168.0.143>
2020-01-26 06:07:01 +0100

    Kommentare und Kosmetik

--- AutonomousStore/src/application/SQLDBReadWrite.java
+++ AutonomousStore/src/application/SQLDBReadWrite.java
@@ -16,49 +16,28 @@
 	                        + "password=showmesql;"
 	                        + "encrypt=false;"
 	                        + "trustServerCertificate=false;"
-	                        + "loginTimeout=30;";
-	        
-	        
-	        
+	                        + "loginTimeout=30;";	        	        	        
 	        
 	       tinsertSql  = "INSERT INTO Kundendaten (tVorname,tNachname,tAdresse,tPLZ,tOrt) VALUES " //,dGeburtsdatum
-	                + "('"+tVorname+ "','"+tNachname+ "','"+tAdresse+ "','"+tPLZ+ "','"+tOrt+ "');"; //,'"+dGeburtsdatum+ "'
-	        
+	                + "('"+tVorname+ "','"+tNachname+ "','"+tAdresse+ "','"+tPLZ+ "','"+tOrt+ "');"; //,'"+dGeburtsdatum+ "'       
 
 	        try (Connection connection = DriverManager.getConnection(tconnectionUrl);
 	                PreparedStatement prepsInsertProduct = connection.prepareStatement(tinsertSql, Statement.RETURN_GENERATED_KEYS);) { 
 	        		
+	        	//SQL Query ausführen
 	        		prepsInsertProduct.execute();
 	                
 	            
-	            // Inserted ID abfüllen
-	            resultSet = prepsInsertProduct.getGeneratedKeys();
+	            // get Inserted ID 
+	        		resultSet = prepsInsertProduct.getGeneratedKeys();
 
-	            // anzeige Inserted ID in Konsole
-	            while (resultSet.next()) {
-	                System.out.println("Generated ID: " + resultSet.getString(1));
+	            // Anzeige Inserted ID in Konsole
+	        		while (resultSet.next()) {
+	        			System.out.println("Generated ID: " + resultSet.getString(1));
 	            }
 	        }
-	        
-//	        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-//	            System.out.println("HelloDB");
-//	            
-//	            //Erstellen des Statement in welches der Query reingeschrieben werden kann
-//	            Statement statement = connection.createStatement(); {
-//
-//	                // Erstellen und Ausführen des Query
-//	                String selectSql = "SELECT * FROM T240_Kunden";
-//	                resultSet = statement.executeQuery(selectSql);
-//
-//	                // Print results from select statement
-//	                while (resultSet.next()) {
-//	                    System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5) + " " + resultSet.getString(6));
-//	                }
-//	            }
-//	        }
-	        // Handle any errors that may have occurred.
-	        catch (SQLException e) {
-	            e.printStackTrace();
+	        	catch (SQLException e) {
+	        		e.printStackTrace();
 	        }
 	    }
 	}
