878a93ced1d865cc30ea2e82364d1cabd8a16a57
EmanuelConstanti <EmanuelConstanti@LWZHNBEC01.home>
2020-01-26 23:24:04 +0100

    TEST

--- AutonomousStore/src/application/ZigarettenController.java
+++ AutonomousStore/src/application/ZigarettenController.java
@@ -2,8 +2,13 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ZigarettenController {
 
@@ -16,6 +21,21 @@ public class ZigarettenController {
     @FXML
     void btnZigarettenWarenkorbHinzu_clicked(ActionEvent event) {
     	txtMengeproAuswahl.setText("Hello World");
+    }
+    
+    @FXML
+    void btnZurueckZuProdAusVONZigare_clicked(ActionEvent event) {
+    	try {				
+    		Stage StageRegist = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			Parent root = FXMLLoader.load(getClass().getResource("ProdukteAuswahl.fxml"));
+			Scene scene = new Scene(root);
+			StageRegist.setScene(scene);
+			StageRegist.show();
+    		} 
+		
+		catch(Exception e) {
+			e.printStackTrace();
+	}	
     }
 
 }
